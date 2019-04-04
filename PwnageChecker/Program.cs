5ae32648404a42de0dd166211d54d8166fc939c6
d0dders <dodders@outlook.com>
2019-04-04 22:23:49 +0100

    Add project files.

---
+++ PwnageChecker/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PwnageChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter and address to check if it has been PWNED!");
+            
+
+
+            var results = GetPwnage(Console.ReadLine());
+            foreach (PwnResult result in results)
+            {
+                Console.WriteLine(result.ToString() + Environment.NewLine);
+                
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<PwnResult> GetPwnage(string account)
+        {
+            //TODO: handle if account was not PWNED: System.Net.WebException: 'The remote server returned an error: (404) Not Found.' 
+            var results = new List<PwnResult>();
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("api-version", "2");
+            webClient.Headers.Add(HttpRequestHeader.UserAgent, "PwnageChecker via Command Line");
+            Byte[] pwnageData = webClient.DownloadData("https://haveibeenpwned.com/api/v2/breachedaccount/" + account);
+
+            using (var stream = new MemoryStream(pwnageData))
+            using (var reader = new StreamReader(stream))
+            {
+                results = JsonConvert.DeserializeObject<List<PwnResult>>(reader.ReadToEnd());
+            }
+            return results;
+        }
+
+    }
+}
---
+++ PwnageChecker/PwnSearch.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PwnageChecker
+{
+
+    public class PwnSearch
+    {
+        [JsonProperty(PropertyName = "Property1")]
+        public List<PwnResult> PwnResults { get; set; }
+    }
+
+    public class PwnResult
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Domain { get; set; }
+        public string BreachDate { get; set; }
+        public string AddedDate { get; set; }
+        public string ModifiedDate { get; set; }
+        public int PwnCount { get; set; }
+        public string Description { get; set; }
+        public string[] DataClasses { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Title: {1}, Domain: {2}, Breach Date: {3}, Added Date: {4}, Modified Date: {5}, Pwn Count: {6}, Description: {7}",
+                Name, Title, Domain, BreachDate, AddedDate, ModifiedDate, PwnCount, Description);
+        }
+    }
+
+
+
+}
